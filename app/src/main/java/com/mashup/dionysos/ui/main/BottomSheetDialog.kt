2fabc0940da53ea1d5116481d4efc74acafef176
yuchocopie <pointu341@gmail.com>
2020-09-05 22:17:32 +0900

    refactor

--- app/src/main/java/com/mashup/dionysos/ui/main/BottomSheetDialog.kt
+++ app/src/main/java/com/mashup/dionysos/ui/main/BottomSheetDialog.kt
@@ -1,14 +1,12 @@
 package com.mashup.dionysos.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.mashup.dionysos.BR
 import com.mashup.dionysos.R
 import com.mashup.dionysos.databinding.BottomSheetLayoutBinding
 
@@ -18,30 +16,16 @@ class BottomSheetDialog : BottomSheetDialogFragment() {
     lateinit var timeViewModel: TimeViewModel
     lateinit var binding: BottomSheetLayoutBinding
 
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater,
             R.layout.bottom_sheet_layout, container, false)
         binding.lifecycleOwner = this
+        Log.e("BottomSheetDialog", ":  onCreateView")
         return binding.root
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        val viewModelFactory =
-                ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
-        timeViewModel = ViewModelProvider(activity!!, viewModelFactory).get(TimeViewModel::class.java)
-        binding.setVariable(BR.timeVM, timeViewModel)
-        timeViewModel.timeLaps.observe(this, Observer { it ->
-            dismiss()
-        })
-
-
     }
 
     override fun getTheme(): Int =
         R.style.BottomSheetDialogTheme
-
 
     companion object {
         val instance: BottomSheetDialog
