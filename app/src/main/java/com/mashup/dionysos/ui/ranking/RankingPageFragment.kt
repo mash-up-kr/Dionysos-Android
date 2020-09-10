9efda4b354e1cea60ccbbcb880060d11a7fd6a36
yuchocopie <pointu341@gmail.com>
2020-09-10 20:17:45 +0900

    fix: deplecated 된 코드 바꾸기

--- app/src/main/java/com/mashup/dionysos/ui/ranking/RankingPageFragment.kt
+++ app/src/main/java/com/mashup/dionysos/ui/ranking/RankingPageFragment.kt
@@ -5,12 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.mashup.dionysos.R
 import com.mashup.dionysos.model.Ranking
-import com.mashup.dionysos.model.StatisticsGraph
-import com.mashup.dionysos.ui.mypage.StatisticsGraphFragment
-import com.mashup.dionysos.ui.mypage.adapter.StatisticsGraphAdapter
 import kotlinx.android.synthetic.main.statistics_graph_fragment.*
 
 class RankingPageFragment : Fragment() {
@@ -30,7 +27,7 @@ class RankingPageFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(RankingPageViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(RankingPageViewModel::class.java)
         // TODO: Use the ViewModel
 
         recyclerview.adapter = RankingAdapter()
