decbc4a3e9d8347759eb389163f82ce9fc9be91b
yuchocopie <pointu341@gmail.com>
2020-10-04 23:29:34 +0900

    로그인로직 디자인 수정 반영

--- app/src/main/java/com/mashup/dionysos/ui/login/WelcomActivity.kt
+++ app/src/main/java/com/mashup/dionysos/ui/login/WelcomActivity.kt
@@ -1,9 +1,8 @@
 package com.mashup.dionysos.ui.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bumptech.glide.Glide
+import androidx.appcompat.app.AppCompatActivity
 import com.mashup.dionysos.R
 import com.mashup.dionysos.ui.main.MainActivity
 import kotlinx.android.synthetic.main.activity_welcom.*
@@ -14,8 +13,6 @@ class WelcomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcom)
-
-        Glide.with(this).load(R.drawable.animation_signup).into(gif_img)
 
         gif_img.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
